b57e5579805a9118d170a1b9e6ad9cea55f9a876
Yigit S <55548182+yigitsr@users.noreply.github.com>
2021-12-21 17:35:56 +0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,4 @@
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', () => { navUL.classList.toggle('show'); });
